dc2bab4da039b5fdc386d8a59164b7690f73d526
knicked <kn1ck3d@gmail.com>
2019-01-06 21:55:38 +0100

    Update block ID definitions

--- src/block.h
+++ src/block.h
@@ -9,6 +9,21 @@
 #define DIRT 3
 #define COBBLESTONE 4
 #define WOOD_PLANKS 5
+#define SAPLING 6
+#define BEDROCK 7
+#define FLOWING_WATER 8
+#define STILL_WATER 9
+#define FLOWING_LAVA 10
+#define STILL_LAVA 11
+#define SAND 12
+#define GRAVEL 13
+#define GOLD_ORE 14
+#define IRON_ORE 15
+#define COAL_ORE 16
+#define WOOD 17
+#define LEAVES 18
+#define SPONGE 19
+#define GLASS 20
 
 typedef unsigned char block_id;
 
