954445812e9df037ddaa80d91653ec7b4a6f40df
knicked <kn1ck3d@gmail.com>
2019-01-06 21:24:45 +0100

    Enable alpha blending

--- res/shaders/blocks.fsh
+++ res/shaders/blocks.fsh
@@ -9,4 +9,6 @@ out vec4 out_color;
 void main()
 {
     out_color = texture(tex, pass_texCoord);
+    if (out_color.a == 0.0)
+        discard;
 }
--- src/game.c
+++ src/game.c
@@ -84,6 +84,8 @@ void game_init(game *g, GLFWwindow *window)
     glEnable(GL_CULL_FACE);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_MULTISAMPLE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     world_init(&g->w);
 
